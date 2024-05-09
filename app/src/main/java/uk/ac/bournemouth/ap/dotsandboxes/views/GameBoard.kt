ce6eb103fb4f14bdf6a191a3f8fe41cc29be8f10
JoeyPBU <128998534+JoeyPBU@users.noreply.github.com>
2024-05-09 13:45:49 +0100

    Grid Displaying

--- app/src/main/java/uk/ac/bournemouth/ap/dotsandboxes/views/GameBoard.kt
+++ app/src/main/java/uk/ac/bournemouth/ap/dotsandboxes/views/GameBoard.kt
@@ -6,10 +6,20 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import org.example.student.dotsboxgame.SimpleRobotPlayer
 import org.example.student.dotsboxgame.StudentDotsBoxGame
+import uk.ac.bournemouth.ap.dotsandboxeslib.Player
 import uk.ac.bournemouth.ap.lib.matrix.Matrix
 import uk.ac.bournemouth.ap.lib.matrix.SparseMatrix
-
+import uk.ac.bournemouth.ap.dotsandboxeslib.AbstractDotsAndBoxesGame.AbstractBox
+import uk.ac.bournemouth.ap.dotsandboxeslib.AbstractDotsAndBoxesGame.AbstractLine
+import uk.ac.bournemouth.ap.dotsandboxeslib.ComputerPlayer
+import uk.ac.bournemouth.ap.dotsandboxeslib.DotsAndBoxesGame
+import uk.ac.bournemouth.ap.dotsandboxeslib.DotsAndBoxesGame.Box
+import uk.ac.bournemouth.ap.dotsandboxeslib.DotsAndBoxesGame.Line
+import uk.ac.bournemouth.ap.dotsandboxeslib.HumanPlayer
+import uk.ac.bournemouth.ap.lib.matrix.ext.Coordinate
+import kotlin.random.Random
 
 class GameBoard @JvmOverloads constructor(
     context: Context,
@@ -17,14 +27,26 @@ class GameBoard @JvmOverloads constructor(
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
 
-    private val linePaint = Paint().apply {
+    private val baseLinePaint = Paint().apply {
+        color = Color.GRAY
+        strokeWidth = 5f
+    }
+
+    private val drawnLinePaint = Paint().apply {
         color = Color.BLACK
+        strokeWidth = 8f
+    }
+
+    private val circlePaint = Paint().apply{
+        color = Color.RED
         strokeWidth = 10f
     }
 
-    var boardOffset: Float = 10f
-    lateinit var game: StudentDotsBoxGame
 
+
+    private var boardOffset: Float = 10f
+    private lateinit var game: StudentDotsBoxGame
+    class Player() : HumanPlayer
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
@@ -32,37 +54,35 @@ class GameBoard @JvmOverloads constructor(
     }
 
 
-    fun drawBoard(canvas: Canvas) {
+    private fun drawBoard(canvas: Canvas) {
         drawGrid(canvas)
         drawBoxes(canvas, 5F)
     }
 
-    fun drawGrid(canvas: Canvas) {
-        val columns = game.lines.maxWidth - 1
-        val rows = (game.lines.maxHeight - 1) / 2
+    private fun drawGrid(canvas: Canvas) {
+        val columns = 6
+        val rows = 6
 
-        val boxWidth = (width - 2 * boardOffset) / columns
-        val boxHeight = (height - 2 * boardOffset) / rows
+        val boxWidth = (width - (columns + 1) * boardOffset) / columns
+        val boxHeight = (height - (rows + 1) * boardOffset) / rows
 
-        for (x in 0 until columns + 1) {
-            val startX = boardOffset + x * boxWidth
-            val startY = boardOffset
-            val endX = startX
-            val endY = boardOffset + rows * boxHeight
-            canvas.drawLine(startX, startY, endX, endY, linePaint)
+        for (x in 0..columns) {
+            for (y in 0..rows) {
+                val startX = boardOffset + x * (boxWidth + boardOffset)
+                val endX = startX + boxWidth
+                val startY = boardOffset + y * (boxHeight + boardOffset)
+                val endY = startY + boxHeight
+
+                val line = game.lines[Coordinate(x, y)]
+                val linePaint = if(line.isDrawn) drawnLinePaint else baseLinePaint
+                canvas.drawLine(startX, startY, endX, startY, linePaint)
+                canvas.drawLine(startX, startY, startX, endY, linePaint)
+                canvas.drawCircle(startX, startY, 10f, circlePaint)
+            }
         }
-
-        for (y in 0 until rows + 1){
-            val startX = boardOffset
-            val startY = boardOffset + y * boxHeight
-            val endX = boardOffset + columns * boxWidth
-            val endY = startY
-            canvas.drawLine(startX, startY, endX, endY, linePaint)
-        }
-
     }
 
-    fun drawBoxes(canvas: Canvas, gridSize: Float) {
+    private fun drawBoxes(canvas: Canvas, gridSize: Float) {
 
     }
 }
